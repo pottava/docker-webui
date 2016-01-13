f030f3e7c7f30c305a1d9c29fd05aad9726ea5e6
ryo nakamaru <pottava@gmail.com>
2016-01-13 12:40:26 +0900

    add usage & make its docker image more lightweight

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,6 @@ var autoprefixer = require("gulp-autoprefixer");
 var plumber = require("gulp-plumber");
 var rename = require('gulp-rename');
 var sass = require("gulp-sass");
-// var sourcemaps = require('gulp-sourcemaps');
 var uglify = require("gulp-uglify");
 var reactify = require('reactify');
 var through  = require('through2');
@@ -25,18 +24,10 @@ gulp.task("react", function() {
   gulp.src('./app/assets/js/**/*.jsx')
       .pipe(browserified)
       .pipe(rename({extname: '.js'}))
-      // .pipe(gulp.dest('./app/assets/js'));
       .pipe(uglify())
       .pipe(gulp.dest("./app/assets/js/min"));
 });
-// gulp.task("js", function() {
-//   gulp.src(["./app/assets/js/**/*.js", "!./app/assets/js/lib/*.js", "!./app/assets/js/min/**/*.js"])
-//       .pipe(plumber())
-//       .pipe(uglify())
-//       .pipe(sourcemaps.init({loadMaps: true}))
-//       .pipe(sourcemaps.write('./'))
-//       .pipe(gulp.dest("./app/assets/js/min"));
-// });
+
 gulp.task("sass", function() {
   gulp.src("./app/assets/scss/**/*.scss")
       .pipe(plumber())
@@ -47,6 +38,5 @@ gulp.task("sass", function() {
 
 gulp.task("default", function() {
     gulp.watch("./app/assets/js/**/*.jsx", ["react"]);
-    // gulp.watch(["./app/assets/js/**/*.js", "!./app/assets/js/lib/*.js", "!./app/assets/js/min/**/*.js"], ["js"]);
     gulp.watch("./app/assets/scss/**/*.scss", ["sass"]);
 });
